7436509cc08d9c41c143de2255f92f9d7233b019
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-07-25 18:17:40 -0400

    Remove Proper from tests to clarify GPL distinction.

--- test/pb.erl
+++ test/pb.erl
@@ -1,65 +1,63 @@
 -module(pb).
 -author('Joseph Abrahamson <me@jspha.com>').
 
--include_lib("proper/include/proper.hrl").
+-include_lib("eqc/include/eqc.hrl").
 -include_lib("eunit/include/eunit.hrl").
+
+-export([prop_encdec_msg/0]).
 
 -include("include/zeta.hrl").
 
-n(G) -> union([undefined, G]).
+n(G) -> elements([undefined, G]).
 bstring() -> binary().
 nstring() -> n(bstring()).
-nfloat() -> n(?LET(Int, integer(), float(Int))).
-nbool() -> n(boolean()).
+nfloat() -> n(?LET(Int, int(), float(Int))).
+nbool() -> n(bool()).
 zeta_state() ->
-    #zeta_state{time = pos_integer(), 
-		state = nstring(), 
-		service = nstring(),
-		host = nstring(), 
-		description = nstring(), 
-		once = nbool(),
-		tags = list(bstring()),
-		ttl = nfloat(),
-		metric_f = nfloat()}.
+  ?LET({Time, State, Service, Host,
+        Once,
+        Desc, Tags, TTL, Metric},
+       {largeint(), nstring(), nstring(), nstring(),
+        nbool(),
+        nstring(), list(bstring()), nfloat(), nfloat()},
+       #zeta_state{time = Time,
+                   state = State,
+                   service = Service,
+                   host = Host,
+                   description = Desc,
+                   once = Once,
+                   tags = Tags,
+                   ttl = TTL,
+                   metric_f = Metric}).
 
 zeta_event() ->
-    #zeta_event{time = pos_integer(), 
-		state = nstring(), 
-		service = nstring(),
-		host = nstring(), 
-		description = nstring(), 
-		tags = list(bstring()),
-		ttl = nfloat(),
-		metric_f = nfloat()}.
+  ?LET({Time, State, Service, Host,
+        Desc, Tags, TTL, Metric},
+       {largeint(), nstring(), nstring(), nstring(),
+        nstring(), list(bstring()), nfloat(), nfloat()},
+       #zeta_event{time = Time,
+                   state = State,
+                   service = Service,
+                   host = Host,
+                   description = Desc,
+                   tags = Tags,
+                   ttl = TTL,
+                   metric_f = Metric}).
 
-zeta_query() -> #zeta_query{string = nstring()}.
+zeta_query() -> ?LET(String, nstring(), #zeta_query{string = String}).
 
 zeta_msg() ->
-    #zeta_msg{ok = nbool(),
-	      error = nstring(),
-	      zstates = list(zeta_state()),
-	      zquery = zeta_query(),
-	      zevents = list(zeta_event())}.
-
-% prop_encdec_state() ->
-%     ?FORALL(State, zeta_state(), 
-% 	    equals(zeta_pb:decode(zeta_pb:encode(State), #zeta_state{}),
-% 		   State)).
-
-% prop_encdec_event() ->
-%     ?FORALL(Event, zeta_event(), 
-% 	    equals(zeta_pb:decode(zeta_pb:encode(Event), #zeta_event{}),
-% 		   Event)).
-
-% prop_encdec_q() ->
-%     ?FORALL(Q, zeta_query(), 
-% 	    equals(zeta_pb:decode(zeta_pb:encode(Q), #zeta_query{}),
-% 		   Q)).
-
+  ?LET({Ok, Error, ZStates, ZQuery, ZEvents},
+       {bool(), nstring(), list(zeta_state()), zeta_query(), list(zeta_event())},
+       #zeta_msg{ok = Ok,
+                 error = Error,
+                 zstates = ZStates,
+                 zquery = ZQuery,
+                 zevents = ZEvents}).
 prop_encdec_msg() ->
-    ?FORALL(Msg, zeta_msg(), 
-	    equals(zeta_pb:decode(zeta_pb:encode(Msg)),
-		     Msg)).
+  ?FORALL(Msg, zeta_msg(), 
+          equals(zeta_pb:decode(zeta_pb:encode(Msg)),
+                 Msg)).
 
 proper_test_() ->
-    [fun () -> [] = proper:module(?MODULE, [verbose, {numtests, 100}]) end].
+  [fun () -> [] = eqc:module(?MODULE) end].
